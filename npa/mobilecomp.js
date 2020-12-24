7e14e6522556beb0209822c90414bdb589c0476f
Ittussarom Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-12-23 21:10:36 -0500

    Update mobilecomp.js

--- npa/mobilecomp.js
+++ npa/mobilecomp.js
@@ -1,5 +1,5 @@
 function isTouchDevice() { return 'ontouchstart' in window || !!(navigator.msMaxTouchPoints);} //https://stackoverflow.com/questions/4817029/whats-the-best-way-to-detect-a-touch-screen-device-using-javascript#comment27386557_13470899
 
 if (isTouchDevice()) {
-console.log("You've reached a dead end");
+$('.invisbutton').css('pointer-events', 'auto') //requires jquery
 }
