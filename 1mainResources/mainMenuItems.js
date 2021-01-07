41a14cb091340aef62a518d88c393a9b3af06743
Ittussarom Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2021-01-07 18:06:35 -0500

    add link to improved youtube page.

--- 1mainResources/mainMenuItems.js
+++ 1mainResources/mainMenuItems.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-//    $(".menuLink1").text("THIS IS BETA, GO BACK TO NON BETA", "");
+//    $(".menuLink1").text("THIS IS BETA, GO BACK TO NON BETA", ""); 
     $(".menuLink2").text("General Stuff", "");
     $(".menuLink3").text("Elder Scrolls Online", "");
     $(".menuLink4").text("Minecraft Server", "");
@@ -17,7 +17,7 @@ $(document).ready(function(){
     $(".menuLink4").attr("href", "minecraft/index.html");
     $(".menuLink5").attr("href", "school/index.html");
     $(".menuLink6").attr("href", "coding/index.html");
-    $(".menuLink7").attr("href", "youtube/yt.html");
+    $(".menuLink7").attr("href", "youtube/index.html");
     $(".menuLink8").attr("href", "contact.html");
     $(".menuLink9").attr("href", "about.html");
     $(".menuLink10").attr("href", "cr/index.html");
