97fa9b4d3f10801288341629c33f6c89cb6e5196
TRM <owen.daigle@outlook.com>
2021-01-29 12:21:37 -0500

    ESO section dropdown, works, but need a way to import content

---
+++ 1mainResources/menuModal.js
@@ -0,0 +1,14 @@
+// Get the modal
+var modal = document.getElementById("menuModal");
+// Get the button that opens the modal
+var btn = document.getElementById("menuModalButton");
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
