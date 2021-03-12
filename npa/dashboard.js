02ca151f15e152baa006eabb526ae34f2f4d6333
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2021-03-12 10:19:21 -0500

    Add Dashboard Again
    Fixes #43

---
+++ npa/dashboard.js
@@ -0,0 +1,4 @@
+var Misc = `<h2>Dashboard</h2>
+<p>You are the misc person, you have nothing special to do! Just do what is needed.<br>Current Project: <b>Securing the base</b></p>`;
+var Secu = `<h2>Dashboard</h2>
+<p>Your project is <a href="#se">Security</a>. Work on it!<br>Or maybe work on the NPA website.`;
