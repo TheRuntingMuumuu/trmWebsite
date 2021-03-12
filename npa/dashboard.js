daf5771c884855ee08c4419963f6588443dc424b
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2021-03-11 21:52:59 -0500

    YAYYYYYYYYYY DASHBOARD WORKS FOR ME AND TRM!
    fixes #43

---
+++ npa/dashboard.js
@@ -0,0 +1,4 @@
+var Misc = `<h1>Dashboard</h1>
+<p>You are the misc person, you have nothing special to do! Just do what is needed.<br>Current Project: <b>Securing the base</b></p>`;
+var Secu = `<h1>Dashboard</h1>
+<p>Your project is <a href="#se">Security</a>. Work on it!<br>Or maybe work on the NPA website.`;
