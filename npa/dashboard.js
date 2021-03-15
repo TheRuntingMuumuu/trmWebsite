755aa23ec71d6a3b123d50e4448e963c7fa15f9f
TRM <theruntingmuumuu@gmail.com>
2021-03-15 15:21:14 -0400

    format

--- npa/dashboard.js
+++ npa/dashboard.js
@@ -1,5 +1,3 @@
 //https://flaviocopes.com/how-to-create-multiline-string-javascript/
-var Misc = `<h2>Dashboard</h2>
-<p>You are the misc person, you have nothing special to do! Just do what is needed.<br>Current Project: <b>Securing the base</b></p>`;
-var Secu = `<h2>Dashboard</h2>
-<p>Your project is <a href="#se">Security</a>. Work on it!<br>Or maybe work on the NPA website.`;
+var Misc = `<p>You are the misc person, you have nothing special to do! Just do what is needed.<br>Current Project: <b>Securing the base</b></p>`;
+var Secu = `<p>Your project is <a href="#se">Security</a>. Work on it!<br>Or maybe work on the NPA website.`;
