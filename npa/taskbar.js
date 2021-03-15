0409a9dc836a5c0e86b239926a1b7d73315ba911
TRM <theruntingmuumuu@gmail.com>
2021-03-15 16:24:26 -0400

    fixes and framework, reload button disapeares whennot used,menuframework

--- npa/taskbar.js
+++ npa/taskbar.js
@@ -1,39 +1,43 @@
 function mainButton() {
   hideAll(); $('.mainWindow').show(); //To hide all the windows and then to show the one selected
   CurrentIframeThingy = 1; //For the reload button, the 1 makes it so it will not work (no iframe to reload)
+  $('.reload').hide(); //hide the reload button for this window (since it will not work)
   currentWindow = "main"; //For the future (identifies all windows, no use case yet, but maybe useful in future???)
-  document.getElementById("windowSpecificMenu").innerHTML = "New text inside the text element!";
+  document.getElementById("windowSpecificMenu").innerHTML = "";//This text will appear at the left of the screen. IDK What I will add here, but it will be something.
 }
 
 function securityButton() {
   hideAll(); $('.securityWindow').show();
   CurrentIframeThingy = "security";
+  $('.reload').show(); //Show the reload button for this window (since it will work)
   currentWindow = "security";
-  document.getElementById("windowSpecificMenu").innerHTML = "Security <a class='linkCursorr' onclick=''>hi</a>";
-
 }
 
 function todoButton() {
   hideAll(); $('.todoWindow').show();
   CurrentIframeThingy = 'todoi';
+  $('.reload').show(); //Show the reload button for this window (since it will work)
   currentWindow = "todo";
 }
 
 function qrScanButton() {
   hideAll(); $('.qrScanWindow').show();
   CurrentIframeThingy = 1;
+  $('.reload').hide(); //hide the reload button for this window (since it will not work)
   currentWindow = "qrscan";
 }
 
 function qrMakeButton() {
   hideAll(); $('.qrCodeWindow').show();
   CurrentIframeThingy = 1;
+  $('.reload').hide(); //hide the reload button for this window (since it will not work)
   currentWindow = "qrmake";
 }
 
 function linksButton() {
   hideAll(); $('.linksWindow').show();
   CurrentIframeThingy = 1;
+  $('.reload').hide(); //hide the reload button for this window (since it will not work)
   currentWindow = "links";
 }
 
