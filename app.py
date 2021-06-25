123e8829813a2e4fdb7864573ec78176c8c4b6c6
TRM <theruntingmuumuu@gmail.com>
2021-06-24 21:35:26 -0400

    NOT a --> changed main menu code to be flaskified

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request
+import mainmenu
 
 app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-    return render_template('index.html')
+    return render_template('index.html', mainMenuItems=mainmenu.items)
 
 @app.route("/sitemap")
 def nav():
@@ -12,6 +13,16 @@ def nav():
 @app.route("/contact")
 def contactMe():
     return render_template("contact.html")
+@app.route("/minecraft")
+def mc():
+    return render_template("minecraft/index.html")
+@app.route("/eso")
+def eso():
+    return render_template("eso/index.html")
+@app.route("/school")
+def school():
+    return render_template("school/index.html")
+
 @app.after_request
 def add_header(r):#https://stackoverflow.com/questions/34066804/disabling-caching-in-flask
     """
---
+++ mainmenu.py
@@ -0,0 +1,11 @@
+items = {
+    "General": "/general",
+    "Elder Scrolls Online": "/eso",
+    "Minecraft": "/minecraft",
+    "School": "/school",
+    "Programming": "/coding",
+    "Youtube": "/youtube/",
+    "Contact Me": "/contact",
+    "Clash Royale Clan": "/cr",
+    "Privacy Policie": "/privacy",
+}
